b4e7b8e352a5c0ac0611525ffe0541e2a1605a8f
Vladislav Merkulov <hoverjet@gmail.com>
2017-02-06 16:16:06 +0300

    Initial commit

---
+++ test/base_helper.rb
@@ -0,0 +1,19 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'test/unit'
+if RUBY_VERSION >= '1.9.3'
+  require 'debugger'
+end
+require 'mocha/setup'
+require 'rails'
+require 'client_side_validations/config'
+
+module TestApp
+  class Application < Rails::Application
+    config.root = File.dirname(__FILE__)
+    config.active_support.deprecation = :log
+    config.logger = Logger.new(STDOUT)
+  end
+end
+
+module ClientSideValidations; end
